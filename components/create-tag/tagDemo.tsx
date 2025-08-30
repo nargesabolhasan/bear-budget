cf95ad50aa34b0b533ae484db62d5ae4bfb20de6
Narges.Abolhasan <narges.abolhasan@gmail.com>
2025-08-30 23:42:07 +0330

    [fix] update demo styles

--- components/create-tag/tagDemo.tsx
+++ components/create-tag/tagDemo.tsx
@@ -9,19 +9,27 @@ const TagDemo = ({
   name,
   color,
   transactionType,
+  className,
+  dataTagId = "",
 }: TagFormData & {
   demoTitle?: string;
+  className?: string;
+  dataTagId?: string;
 }) => {
   const Icon = iconList.get(icon || "0")?.icon;
   return (
     <div
-      className={
-        "w-full transition-all delay-500 flex flex-col gap-2 mx-auto justify-center items-center bg-gray-100 border border-dashed border-gray-400 p-4 rounded-lg"
-      }
+      className={twMerge(
+        "flex flex-col gap-2 mx-auto justify-center items-center bg-gray-100 border border-dashed border-gray-400 p-4 rounded-lg",
+        className && className
+      )}
+      data-tag-id={dataTagId}
+      dir={"auto"}
     >
       <div className={"flex flex-col gap-2 items-start w-full"}>
         {demoTitle && <h3 className={"flex gap-1 flex-row"}>{demoTitle}</h3>}
       </div>
+      <span className={"block text-md text-dark"}>{name ?? " "}</span>
       <i
         className={twMerge(
           "size-[50px] rounded-full flex justify-center items-center",
@@ -30,8 +38,7 @@ const TagDemo = ({
       >
         {Icon ? <Icon /> : <></>}
       </i>
-      <p className={"mx-auto italic"}> {transactionType}</p>
-      <span className={"block text-md text-gray-900"}>{name ?? " "}</span>
+      <h4 className={"mx-auto text-placeholder"}>{transactionType}</h4>
     </div>
   );
 };
