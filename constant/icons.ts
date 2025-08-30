19369426b583faeeb75e2be24f9691d1cd2af199
Narges.Abolhasan <narges.abolhasan@gmail.com>
2025-08-30 17:44:35 +0330

    [feat] update icons of icon picker

--- constant/icons.ts
+++ constant/icons.ts
@@ -2,11 +2,399 @@ import HomeIcon from "@mui/icons-material/Home";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import StarIcon from "@mui/icons-material/Star";
 import FaceIcon from "@mui/icons-material/Face";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import AirplanemodeActiveIcon from "@mui/icons-material/AirplanemodeActive";
+import AirportShuttleIcon from "@mui/icons-material/AirportShuttle";
+import AppleIcon from "@mui/icons-material/Apple";
+import ArchiveIcon from "@mui/icons-material/Archive";
+import BalanceIcon from "@mui/icons-material/Balance";
+import ChurchIcon from "@mui/icons-material/Church";
+import BathtubIcon from "@mui/icons-material/Bathtub";
+import BatteryChargingFullIcon from "@mui/icons-material/BatteryChargingFull";
+import BeachAccessIcon from "@mui/icons-material/BeachAccess";
+import BedroomChildIcon from "@mui/icons-material/BedroomChild";
+import BlenderIcon from "@mui/icons-material/Blender";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import BorderColorIcon from "@mui/icons-material/BorderColor";
+import Brightness2Icon from "@mui/icons-material/Brightness2";
+import BrushIcon from "@mui/icons-material/Brush";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import CakeIcon from "@mui/icons-material/Cake";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import CallIcon from "@mui/icons-material/Call";
+import CameraAltIcon from "@mui/icons-material/CameraAlt";
+import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
+import CelebrationIcon from "@mui/icons-material/Celebration";
+import ChangeCircleIcon from "@mui/icons-material/ChangeCircle";
+import ChairIcon from "@mui/icons-material/Chair";
+import CardMembershipIcon from "@mui/icons-material/CardMembership";
+import CardTravelIcon from "@mui/icons-material/CardTravel";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import ChildFriendlyIcon from "@mui/icons-material/ChildFriendly";
+import CheckroomIcon from "@mui/icons-material/Checkroom";
+import ChecklistRtlIcon from "@mui/icons-material/ChecklistRtl";
+import CircleNotificationsIcon from "@mui/icons-material/CircleNotifications";
+import CloudIcon from "@mui/icons-material/Cloud";
+import CloudDoneIcon from "@mui/icons-material/CloudDone";
+import CloudySnowingIcon from "@mui/icons-material/CloudySnowing";
+import ColorLensIcon from "@mui/icons-material/ColorLens";
+import CompostIcon from "@mui/icons-material/Compost";
+import ComputerIcon from "@mui/icons-material/Computer";
+import ConnectedTvIcon from "@mui/icons-material/ConnectedTv";
+import ConstructionIcon from "@mui/icons-material/Construction";
+import ConnectingAirportsIcon from "@mui/icons-material/ConnectingAirports";
+import ContentPasteIcon from "@mui/icons-material/ContentPaste";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import CropOriginalIcon from "@mui/icons-material/CropOriginal";
+import CurrencyExchangeIcon from "@mui/icons-material/CurrencyExchange";
+import CurrencyBitcoinIcon from "@mui/icons-material/CurrencyBitcoin";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import DeckIcon from "@mui/icons-material/Deck";
+import DeleteIcon from "@mui/icons-material/Delete";
+import DepartureBoardIcon from "@mui/icons-material/DepartureBoard";
+import DesktopWindowsIcon from "@mui/icons-material/DesktopWindows";
+import DesignServicesIcon from "@mui/icons-material/DesignServices";
+import DeliveryDiningIcon from "@mui/icons-material/DeliveryDining";
+import DiningIcon from "@mui/icons-material/Dining";
+import DirectionsBoatFilledIcon from "@mui/icons-material/DirectionsBoatFilled";
+import DirectionsBusFilledIcon from "@mui/icons-material/DirectionsBusFilled";
+import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
+import DirectionsIcon from "@mui/icons-material/Directions";
+import DirectionsBikeIcon from "@mui/icons-material/DirectionsBike";
+import DirectionsRailwayFilledIcon from "@mui/icons-material/DirectionsRailwayFilled";
+import DirectionsSubwayIcon from "@mui/icons-material/DirectionsSubway";
+import DiscountIcon from "@mui/icons-material/Discount";
+import Diversity1Icon from "@mui/icons-material/Diversity1";
+import Diversity3Icon from "@mui/icons-material/Diversity3";
+import DoDisturbIcon from "@mui/icons-material/DoDisturb";
+import DnsIcon from "@mui/icons-material/Dns";
+import DoDisturbOnIcon from "@mui/icons-material/DoDisturbOn";
+import DownhillSkiingIcon from "@mui/icons-material/DownhillSkiing";
+import DoorbellIcon from "@mui/icons-material/Doorbell";
+import EditIcon from "@mui/icons-material/Edit";
+import ElectricBoltIcon from "@mui/icons-material/ElectricBolt";
+import DryCleaningIcon from "@mui/icons-material/DryCleaning";
+import FaceRetouchingNaturalIcon from "@mui/icons-material/FaceRetouchingNatural";
+import ElectricScooterIcon from "@mui/icons-material/ElectricScooter";
+import ElectricalServicesIcon from "@mui/icons-material/ElectricalServices";
+import EmojiFoodBeverageIcon from "@mui/icons-material/EmojiFoodBeverage";
+import EmergencyRecordingIcon from "@mui/icons-material/EmergencyRecording";
+import EngineeringIcon from "@mui/icons-material/Engineering";
+import EnhancedEncryptionIcon from "@mui/icons-material/EnhancedEncryption";
+import EvStationIcon from "@mui/icons-material/EvStation";
+import ExtensionIcon from "@mui/icons-material/Extension";
+import Face3Icon from "@mui/icons-material/Face3";
+import Face4Icon from "@mui/icons-material/Face4";
+import Face5Icon from "@mui/icons-material/Face5";
+import Face6Icon from "@mui/icons-material/Face6";
+import FamilyRestroomIcon from "@mui/icons-material/FamilyRestroom";
+import FeaturedPlayListIcon from "@mui/icons-material/FeaturedPlayList";
+import FactCheckIcon from "@mui/icons-material/FactCheck";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import FestivalIcon from "@mui/icons-material/Festival";
+import FilterVintageIcon from "@mui/icons-material/FilterVintage";
+import FindInPageIcon from "@mui/icons-material/FindInPage";
+import FlashlightOnIcon from "@mui/icons-material/FlashlightOn";
+import FlipCameraIosIcon from "@mui/icons-material/FlipCameraIos";
+import FlatwareIcon from "@mui/icons-material/Flatware";
+import FmdGoodIcon from "@mui/icons-material/FmdGood";
+import FolderSharedIcon from "@mui/icons-material/FolderShared";
+import ForestIcon from "@mui/icons-material/Forest";
+import FrontHandIcon from "@mui/icons-material/FrontHand";
+import HeadsetIcon from "@mui/icons-material/Headset";
+import HeartBrokenIcon from "@mui/icons-material/HeartBroken";
+import HealthAndSafetyIcon from "@mui/icons-material/HealthAndSafety";
+import HealingIcon from "@mui/icons-material/Healing";
+import HomeWorkIcon from "@mui/icons-material/HomeWork";
+import HotelIcon from "@mui/icons-material/Hotel";
+import HourglassTopIcon from "@mui/icons-material/HourglassTop";
+import IcecreamIcon from "@mui/icons-material/Icecream";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import InsertLinkIcon from "@mui/icons-material/InsertLink";
+import IntegrationInstructionsIcon from "@mui/icons-material/IntegrationInstructions";
+import KeyIcon from "@mui/icons-material/Key";
+import KeyboardVoiceIcon from "@mui/icons-material/KeyboardVoice";
+import MovieFilterIcon from "@mui/icons-material/MovieFilter";
+import MovieCreationIcon from "@mui/icons-material/MovieCreation";
+import MyLocationIcon from "@mui/icons-material/MyLocation";
+import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
+import PersonIcon from "@mui/icons-material/Person";
+import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import CrueltyFreeIcon from "@mui/icons-material/CrueltyFree";
+import ShowerIcon from "@mui/icons-material/Shower";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import SignalWifi2BarIcon from "@mui/icons-material/SignalWifi2Bar";
+import SpaIcon from "@mui/icons-material/Spa";
+import SpatialTrackingIcon from "@mui/icons-material/SpatialTracking";
+import SpeakerIcon from "@mui/icons-material/Speaker";
+import SportsMotorsportsIcon from "@mui/icons-material/SportsMotorsports";
+import SportsBasketballIcon from "@mui/icons-material/SportsBasketball";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import SportsGolfIcon from "@mui/icons-material/SportsGolf";
+import SportsRugbyIcon from "@mui/icons-material/SportsRugby";
+import SportsGymnasticsIcon from "@mui/icons-material/SportsGymnastics";
+import SportsTennisIcon from "@mui/icons-material/SportsTennis";
+import SportsScoreIcon from "@mui/icons-material/SportsScore";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import StraightenIcon from "@mui/icons-material/Straighten";
+import SupportAgentIcon from "@mui/icons-material/SupportAgent";
+import SurfingIcon from "@mui/icons-material/Surfing";
+import SwapHorizontalCircleIcon from "@mui/icons-material/SwapHorizontalCircle";
+import SunnyIcon from "@mui/icons-material/Sunny";
+import SynagogueIcon from "@mui/icons-material/Synagogue";
+import TableBarIcon from "@mui/icons-material/TableBar";
+import TableRestaurantIcon from "@mui/icons-material/TableRestaurant";
+import TempleBuddhistIcon from "@mui/icons-material/TempleBuddhist";
+import TelegramIcon from "@mui/icons-material/Telegram";
+import TextFormatIcon from "@mui/icons-material/TextFormat";
+import TerminalIcon from "@mui/icons-material/Terminal";
+import TapasIcon from "@mui/icons-material/Tapas";
+import TableRowsIcon from "@mui/icons-material/TableRows";
+import TextSnippetIcon from "@mui/icons-material/TextSnippet";
+import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
+import TheatersIcon from "@mui/icons-material/Theaters";
+import ThermostatIcon from "@mui/icons-material/Thermostat";
+import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
+import ThunderstormIcon from "@mui/icons-material/Thunderstorm";
+import TimerIcon from "@mui/icons-material/Timer";
+import TipsAndUpdatesIcon from "@mui/icons-material/TipsAndUpdates";
+import ToysIcon from "@mui/icons-material/Toys";
+import TrafficIcon from "@mui/icons-material/Traffic";
+import TranslateIcon from "@mui/icons-material/Translate";
+import TrolleyIcon from "@mui/icons-material/Trolley";
+import TrendingUpIcon from "@mui/icons-material/TrendingUp";
+import TrendingDownIcon from "@mui/icons-material/TrendingDown";
+import TravelExploreIcon from "@mui/icons-material/TravelExplore";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VolunteerActivismIcon from "@mui/icons-material/VolunteerActivism";
+import VpnKeyIcon from "@mui/icons-material/VpnKey";
+import VolumeUpIcon from "@mui/icons-material/VolumeUp";
+import WcIcon from "@mui/icons-material/Wc";
+import WhatshotIcon from "@mui/icons-material/Whatshot";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import WifiIcon from "@mui/icons-material/Wifi";
+import ZoomInIcon from "@mui/icons-material/ZoomIn";
+import WorkspacesFilledIcon from "@mui/icons-material/WorkspacesFilled";
+import WeekendIcon from "@mui/icons-material/Weekend";
+import NightlifeIcon from "@mui/icons-material/Nightlife";
+import PianoIcon from "@mui/icons-material/Piano";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import MosqueIcon from "@mui/icons-material/Mosque";
+import SavingsIcon from "@mui/icons-material/Savings";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
+import EnergySavingsLeafIcon from "@mui/icons-material/EnergySavingsLeaf";
+import MedicationLiquidIcon from "@mui/icons-material/MedicationLiquid";
+import ScaleIcon from "@mui/icons-material/Scale";
 import { IconOptionMap } from "@/types/global";
 
-export const iconList: IconOptionMap = new Map([
-  ["1", { id: "1", icon: HomeIcon }],
-  ["2", { id: "2", icon: FaceIcon }],
-  ["3", { id: "3", icon: StarIcon }],
-  ["4", { id: "4", icon: FavoriteIcon }],
-]);
+const icons = [
+  HomeIcon,
+  FaceIcon,
+  FavoriteIcon,
+  StarIcon,
+  AccountBalanceWalletIcon,
+  AddShoppingCartIcon,
+  AirplanemodeActiveIcon,
+  AirportShuttleIcon,
+  AppleIcon,
+  ArchiveIcon,
+  BalanceIcon,
+  ChurchIcon,
+  BathtubIcon,
+  BatteryChargingFullIcon,
+  BeachAccessIcon,
+  BedroomChildIcon,
+  BlenderIcon,
+  BookmarkIcon,
+  BorderColorIcon,
+  Brightness2Icon,
+  BrushIcon,
+  BusinessCenterIcon,
+  CakeIcon,
+  CalendarMonthIcon,
+  CallIcon,
+  CameraAltIcon,
+  CardGiftcardIcon,
+  CelebrationIcon,
+  ChangeCircleIcon,
+  ChairIcon,
+  CardMembershipIcon,
+  CardTravelIcon,
+  CheckBoxIcon,
+  CheckCircleIcon,
+  ChildFriendlyIcon,
+  CheckroomIcon,
+  ChecklistRtlIcon,
+  CircleNotificationsIcon,
+  CloudIcon,
+  CloudDoneIcon,
+  CloudySnowingIcon,
+  ColorLensIcon,
+  CompostIcon,
+  ComputerIcon,
+  ConnectedTvIcon,
+  ConstructionIcon,
+  ConnectingAirportsIcon,
+  ContentPasteIcon,
+  CreditCardIcon,
+  CropOriginalIcon,
+  CurrencyExchangeIcon,
+  CurrencyBitcoinIcon,
+  DashboardIcon,
+  DeckIcon,
+  DeleteIcon,
+  DepartureBoardIcon,
+  DesktopWindowsIcon,
+  DesignServicesIcon,
+  DeliveryDiningIcon,
+  DiningIcon,
+  DirectionsBoatFilledIcon,
+  DirectionsBusFilledIcon,
+  DirectionsCarIcon,
+  DirectionsIcon,
+  DirectionsBikeIcon,
+  DirectionsRailwayFilledIcon,
+  DirectionsSubwayIcon,
+  DiscountIcon,
+  Diversity1Icon,
+  Diversity3Icon,
+  DoDisturbIcon,
+  DnsIcon,
+  DoDisturbOnIcon,
+  DownhillSkiingIcon,
+  DoorbellIcon,
+  EditIcon,
+  ElectricBoltIcon,
+  DryCleaningIcon,
+  FaceRetouchingNaturalIcon,
+  ElectricScooterIcon,
+  ElectricalServicesIcon,
+  EmojiFoodBeverageIcon,
+  EmergencyRecordingIcon,
+  EngineeringIcon,
+  EnhancedEncryptionIcon,
+  EvStationIcon,
+  ExtensionIcon,
+  Face3Icon,
+  Face4Icon,
+  Face5Icon,
+  Face6Icon,
+  FamilyRestroomIcon,
+  FeaturedPlayListIcon,
+  FactCheckIcon,
+  FacebookIcon,
+  FestivalIcon,
+  FilterVintageIcon,
+  FindInPageIcon,
+  FlashlightOnIcon,
+  FlipCameraIosIcon,
+  FlatwareIcon,
+  FmdGoodIcon,
+  FolderSharedIcon,
+  ForestIcon,
+  FrontHandIcon,
+  HeadsetIcon,
+  HeartBrokenIcon,
+  HealthAndSafetyIcon,
+  HealingIcon,
+  HomeWorkIcon,
+  HotelIcon,
+  HourglassTopIcon,
+  IcecreamIcon,
+  InsertEmoticonIcon,
+  InstagramIcon,
+  InsertLinkIcon,
+  IntegrationInstructionsIcon,
+  KeyIcon,
+  KeyboardVoiceIcon,
+  MovieFilterIcon,
+  MovieCreationIcon,
+  MyLocationIcon,
+  NotificationsActiveIcon,
+  PersonIcon,
+  RocketLaunchIcon,
+  ShoppingCartIcon,
+  CrueltyFreeIcon,
+  ShowerIcon,
+  ShoppingBasketIcon,
+  SignalWifi2BarIcon,
+  SpaIcon,
+  SpatialTrackingIcon,
+  SpeakerIcon,
+  SportsMotorsportsIcon,
+  SportsBasketballIcon,
+  SportsEsportsIcon,
+  SportsGolfIcon,
+  SportsRugbyIcon,
+  SportsGymnasticsIcon,
+  SportsTennisIcon,
+  SportsScoreIcon,
+  SubscriptionsIcon,
+  StraightenIcon,
+  SupportAgentIcon,
+  SurfingIcon,
+  SwapHorizontalCircleIcon,
+  SunnyIcon,
+  SynagogueIcon,
+  TableBarIcon,
+  TableRestaurantIcon,
+  TempleBuddhistIcon,
+  TelegramIcon,
+  TextFormatIcon,
+  TerminalIcon,
+  TapasIcon,
+  TableRowsIcon,
+  TextSnippetIcon,
+  TheaterComedyIcon,
+  TheatersIcon,
+  ThermostatIcon,
+  ThumbUpAltIcon,
+  ThunderstormIcon,
+  TimerIcon,
+  TipsAndUpdatesIcon,
+  ToysIcon,
+  TrafficIcon,
+  TranslateIcon,
+  TrolleyIcon,
+  TrendingUpIcon,
+  TrendingDownIcon,
+  TravelExploreIcon,
+  TwitterIcon,
+  VisibilityIcon,
+  VolunteerActivismIcon,
+  VpnKeyIcon,
+  VolumeUpIcon,
+  WcIcon,
+  WhatshotIcon,
+  WhatsAppIcon,
+  WifiIcon,
+  ZoomInIcon,
+  WorkspacesFilledIcon,
+  WeekendIcon,
+  NightlifeIcon,
+  PianoIcon,
+  FitnessCenterIcon,
+  MosqueIcon,
+  SavingsIcon,
+  AttachFileIcon,
+  AccountBalanceIcon,
+  EnergySavingsLeafIcon,
+  MedicationLiquidIcon,
+  ScaleIcon,
+];
+
+export const iconListGenerator = (): IconOptionMap => {
+  const list: IconOptionMap = new Map([]);
+  icons.forEach((icon, index) => {
+    list.set(index.toString(), { id: index.toString(), icon: icon });
+  });
+  return list;
+};
+
+export const iconList: IconOptionMap = iconListGenerator();
