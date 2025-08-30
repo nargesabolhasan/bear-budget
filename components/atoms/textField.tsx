09989b22a23139d8458c7b3c07c3b15bf6d69d74
Narges.Abolhasan <narges.abolhasan@gmail.com>
2025-08-30 23:41:31 +0330

    [refactor]add rtl mui

--- components/atoms/textField.tsx
+++ components/atoms/textField.tsx
@@ -4,7 +4,7 @@ import { TextField, TextFieldProps } from "@mui/material";
 export type TextFieldType = Omit<TextFieldProps, "variant"> & {};
 
 const ITextField = ({ ...props }: TextFieldType) => {
-  return <TextField variant={"outlined"} {...props} />;
+  return <TextField variant={"outlined"} {...props} dir={"rtl"} />;
 };
 
 export default ITextField;
