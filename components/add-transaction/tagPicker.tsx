c1c3ccae6afb2f8e135c992b12e44d4a49c698a7
Narges.Abolhasan <narges.abolhasan@gmail.com>
2025-08-30 23:40:04 +0330

    [refactor] use accordion instead of select for tags

--- components/add-transaction/tagPicker.tsx
+++ components/add-transaction/tagPicker.tsx
@@ -1,34 +1,48 @@
 "use client";
 
 import React from "react";
-import { MenuItem, Select, SelectChangeEvent } from "@mui/material";
+import twMerge from "@/utils/utils";
 import { TagType } from "@/types/global";
 import TagDemo from "@/components/create-tag/tagDemo";
 
-interface TagPickerProps {
-  items: TagType[];
-  onChange: (iconId: TagType["id"] | null) => void;
-  value: string | null;
-}
+export type tagPickerType = {
+  tagList: TagType[];
+  onChange: (tagId: TagType["id"]) => void;
+  value: TagType["id"];
+};
 
-const TagPicker = ({ onChange, items, value }: TagPickerProps) => {
-  const handleSelect = (e: SelectChangeEvent<string>) => {
-    onChange(e.target.value || null);
+const TagPicker = ({ tagList, value = "", onChange }: tagPickerType) => {
+  const handleSelect = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    const button = (e.target as HTMLButtonElement).closest(".tag-picker-item");
+    if (button) {
+      const id = button.getAttribute("data-tag-id") || "";
+      onChange(id);
+    }
   };
 
   return (
-    <Select onChange={handleSelect} value={value ?? ""} displayEmpty fullWidth>
-      {items.map((item) => (
-        <MenuItem key={item.id} value={item.id}>
-          <TagDemo
-            icon={item.icon}
-            name={item.name}
-            color={item.color}
-            transactionType={item.transactionType}
-          />
-        </MenuItem>
+    <div
+      onClick={handleSelect}
+      className={twMerge(
+        "tag-wrapper w-full grid grid-cols-[repeat(auto-fill,minmax(100px,1fr))] gap-3"
+      )}
+    >
+      {tagList.map((tag) => (
+        <TagDemo
+          className={twMerge(
+            "tag-picker-item w-[100px] p-2!",
+            value === tag.id &&
+              "bg-placeholder_light border-solid border-2 border-dark!"
+          )}
+          icon={tag.icon}
+          name={tag.name}
+          color={tag.color}
+          transactionType={tag.transactionType}
+          dataTagId={tag.id}
+        />
       ))}
-    </Select>
+    </div>
   );
 };
 
