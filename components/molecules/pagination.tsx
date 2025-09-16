d436fc21af9d51bb7733eb77189117bd356d4513
Narges.Abolhasan <narges.abolhasan@gmail.com>
2025-09-16 13:37:46 +0330

    [feat] implement pagination

--- components/molecules/pagination.tsx
+++ components/molecules/pagination.tsx
@@ -4,12 +4,15 @@ import Stack from "@mui/material/Stack";
 //@ts-ignore
 import { PaginationProps } from "@mui/material/esm/Pagination/Pagination";
 
+export const ROWS_PER_PAGE = 10;
+export const PAGE_COUNT = 10;
+
 export default function IPagination({ ...props }: PaginationProps) {
   return (
     <Stack spacing={2} className={"flex items-center my-5"}>
       <Pagination
         {...props}
-        count={10}
+        count={props.count || PAGE_COUNT}
         variant="outlined"
         color="primary"
         dir={"ltr"}
---
+++ hooks/usePagination.tsx
@@ -0,0 +1,13 @@
+import { useState, useMemo } from "react";
+
+export default function usePaginationData<T>(list: T[], rowsPerPage: number) {
+  const [page, setPage] = useState(1);
+
+  const paginated = useMemo(() => {
+    return list.slice((page - 1) * rowsPerPage, page * rowsPerPage);
+  }, [list, page, rowsPerPage]);
+
+  const pageCount = Math.ceil(list.length / rowsPerPage);
+
+  return { paginated, page, setPage, pageCount };
+}
