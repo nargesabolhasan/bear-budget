6c2fe2ad9e6895024253dbc0783d79c7d65b6274
Narges.Abolhasan <narges.abolhasan@gmail.com>
2025-09-24 18:53:11 +0330

    [feat]add hint to text filed

--- components/atoms/textField.tsx
+++ components/atoms/textField.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { TextField, TextFieldProps } from "@mui/material";
 
-export type TextFieldType = Omit<TextFieldProps, "variant"> & {};
+export type TextFieldType = Omit<TextFieldProps, "variant"> & {
+  hint?: string;
+  showHint?: boolean;
+};
 
 const ITextField = ({ ...props }: TextFieldType) => {
-  return <TextField variant={"outlined"} {...props} dir={"rtl"} />;
+  return (
+    <>
+      <TextField variant={"outlined"} {...props} dir={"rtl"} />
+      {props.showHint && (
+        <p className={"text-xs text-placeholder"}>{props.hint}</p>
+      )}
+    </>
+  );
 };
 
 export default ITextField;
