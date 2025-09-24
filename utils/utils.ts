34139c8003bb8bbd5b7d3d419fbfbd2e2674b5b0
Narges.Abolhasan <narges.abolhasan@gmail.com>
2025-09-24 16:29:38 +0330

    [fix]fix show nan when type letter

--- utils/utils.ts
+++ utils/utils.ts
@@ -5,6 +5,11 @@ export default function twMerge(...parts: Parts) {
 }
 
 export const convertToCurrency = (currency: number | string): string => {
-  if (currency === "" || currency === null || currency === undefined) return "";
+  if (currency === null || currency === undefined) return "";
+
+  const numericValue =
+    typeof currency === "string" ? Number(currency) : currency;
+
+  if (isNaN(numericValue) || !isFinite(numericValue)) return "";
   return new Intl.NumberFormat("en-US").format(Number(currency));
 };
