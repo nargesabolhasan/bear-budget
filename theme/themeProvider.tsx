3fd3c51cff66cee7b5c3adc0f5ac7277ba18c363
Narges.Abolhasan <narges.abolhasan@gmail.com>
2025-11-29 21:53:15 +0330

    [feat] update theme provider

--- theme/themeProvider.tsx
+++ theme/themeProvider.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { ThemeProvider as MuiThemeProvider, CssBaseline } from "@mui/material";
 import theme from "./theme";
 import { CacheProvider } from "@emotion/react";
 import rtlPlugin from "@mui/stylis-plugin-rtl";
 import { prefixer } from "stylis";
 import createCache from "@emotion/cache";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 interface ThemeProviderProps {
   children: ReactNode;
@@ -17,12 +18,15 @@ export default function IThemeProvider({ children }: ThemeProviderProps) {
     key: "muirtl",
     stylisPlugins: [prefixer, rtlPlugin],
   });
+  const [queryClient] = useState(() => new QueryClient());
 
   return (
     <CacheProvider value={cacheRtl}>
       <MuiThemeProvider theme={theme}>
         <CssBaseline />
-        {children}
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
       </MuiThemeProvider>
     </CacheProvider>
   );
