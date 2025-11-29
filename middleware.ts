c7bad006ef7163cffb87c35158fd9b093d034334
Narges.Abolhasan <narges.abolhasan@gmail.com>
2025-11-29 21:51:34 +0330

    [feat] Generate middleware to check user login

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+// middleware.ts
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get("username")?.value;
+  const isLoggedIn = Boolean(token);
+  const isAuthPage = req.nextUrl.pathname.startsWith("/login");
+
+  if (!isLoggedIn && !isAuthPage) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  if (isLoggedIn && isAuthPage) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!_next|api|login|favicon.ico|fonts|images|robots.txt).*)"],
+};
