229323629c1b17c82a2343f95c83c39ecaa813f9
Narges.Abolhasan <narges.abolhasan@gmail.com>
2025-11-30 00:40:07 +0330

    [feat] add manifest file

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import IHeader from "@/components/molecules/header";
 import { navItems } from "@/constant/navItems";
 import DialogContainer from "@/components/molecules/dialogContainer";
 import IThemeProvider from "@/theme/themeProvider";
-import Script from "next/script";
 
 export const metadata = {
   title: "Bear Budget",
@@ -18,6 +17,17 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        {/* Manifest */}
+        <link rel="manifest" href="/manifest.json" />
+        {/* Apple PWA Icon (Safari) */}
+        <link rel="apple-touch-icon" href="/setting.png" />
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        {/* PWA for iOS */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+      </head>
       <body className={"!bg-neutral_light h-screen flex flex-col"}>
         <IThemeProvider>
           <IHeader navItems={navItems} />
