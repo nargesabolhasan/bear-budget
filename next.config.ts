9c3e051950dbaca5bb56e2576e6e3f27d284bba7
Narges.Abolhasan <narges.abolhasan@gmail.com>
2025-12-29 23:29:48 +0330

    update safari image preview

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    dangerouslyAllowSVG: true,
+    formats: ["image/avif", "image/webp"],
+
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "bear-budget-production.up.railway.app",
+      },
+    ],
+  },
+  experimental: {
+    scrollRestoration: true,
+  },
 };
 
 export default nextConfig;
