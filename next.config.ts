423bd8cfd2053f0672f1ddd8b82e177546af4746
Narges.Abolhasan <narges.abolhasan@gmail.com>
2025-12-30 00:09:30 +0330

    rest next config

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  output: "standalone",
+import type { NextConfig } from "next";
 
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "bear-budget-production.up.railway.app",
-        pathname: "/**",
-      },
-    ],
-  },
-
-  async headers() {
-    return [
-      {
-        source: "/:all*(svg|jpg|png|gif|webp|avif)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
