d57d345b30fba65bfaf96a02e71917bb9430fd1b
Narges.Abolhasan <narges.abolhasan@gmail.com>
2026-01-28 23:27:13 +0330

    fix:redesign scroll to bottom

--- components/molecules/scrollToBottom.tsx
+++ components/molecules/scrollToBottom.tsx
@@ -1,65 +1,38 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { Fab } from "@mui/material";
 import KeyboardDoubleArrowDownTwoToneIcon from "@mui/icons-material/KeyboardDoubleArrowDownTwoTone";
 import { twMerge } from "tailwind-merge";
 
-const ScrollToBottomButton = () => {
-  const [isAtBottom, setIsAtBottom] = useState(false);
+const ScrollToTopButton = () => {
   const [showButton, setShowButton] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      const isAtBottom =
-        window.innerHeight + window.scrollY >=
-        document.documentElement.scrollHeight - 50;
-
-      setIsAtBottom(!isAtBottom);
-      setShowButton(document.documentElement.scrollHeight > window.innerHeight);
+      setShowButton(window.scrollY > 80);
     };
 
-    window.addEventListener("scroll", handleScroll);
     handleScroll();
+    window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
-
-  const scrollToBottom = () => {
-    window.scrollTo({
-      top: document.documentElement.scrollHeight,
-      behavior: "smooth",
-    });
-  };
-  if (!showButton) return null;
 
   return (
-    <Fab
-      onClick={!isAtBottom ? scrollToTop : scrollToBottom}
-      sx={{
-        position: "fixed",
-        bottom: "calc(16px + env(safe-area-inset-bottom))",
-        left: {
-          xs: 16,
-          lg: "30%",
-        },
-        boxShadow: "0px 0px 2px 8px rgba(255, 255, 255, 0.8)",
-        zIndex: (theme) => theme.zIndex.fab,
-      }}
-      aria-label={isAtBottom ? "scroll-to-top" : "scroll-to-bottom"}
-      className={"!border-3 !border-olive_darb print:!hidden"}
+    <div
+      onClick={scrollToTop}
+      aria-label="scroll-to-top"
+      className={twMerge(
+        "w-fit bg-primary_light rounded-full p-3 !border-6 !border-primary print:!hidden drop-shadow-sm fixed bottom-4 right-4 transition-opacity opacity-100",
+        !showButton && "opacity-0 pointer-events-none"
+      )}
     >
-      <KeyboardDoubleArrowDownTwoToneIcon
-        className={twMerge("text-olive_darb", !isAtBottom && "rotate-180")}
-      />
-    </Fab>
+      <KeyboardDoubleArrowDownTwoToneIcon className="text-olive_darb rotate-180" />
+    </div>
   );
 };
 
-export default ScrollToBottomButton;
+export default ScrollToTopButton;
