fe4fcd6031044399057fda8521e1b71488e81666
nargesabolhasan <narges.abolhasan@gmail.com>
2026-06-19 23:51:05 +0330

    fix :updage manifest and layout metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,10 @@ import { FilteredDateProvider } from "@/context/filteredDateContext";
 export const metadata = {
   title: "Bear Budget",
   description: "You can manage your transaction with Bear Budget :)",
+  manifest: "/manifest.json",
   icons: {
-    icon: "/favicon.svg",
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
   },
 };
 
@@ -24,37 +26,26 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         {/* PWA Manifest */}
         <link rel="manifest" href="/manifest.json" />
 
-        {/* Favicon */}
-        <link rel="icon" href="/favicon.svg" />
-        <link rel="shortcut icon" href="/favicon.svg" />
-
-        {/* Apple Touch Icons */}
-        <link rel="apple-touch-icon" sizes="180x180" href="/setting.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/setting.png" />
-        <link rel="apple-touch-icon" sizes="120x120" href="/setting.png" />
-        <link rel="apple-touch-icon" sizes="76x76" href="/setting.png" />
-
-        {/* Android Chrome Icons */}
-        <link rel="icon" type="image/png" sizes="192x192" href="/setting.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="/setting.png" />
-
         {/* Theme color */}
-        <meta name="theme-color" content="#000000" />
+        <meta name="theme-color" content="#9ab973" />
 
         {/* Apple PWA meta */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
       </head>
+
       <body className="!bg-neutral_light h-screen flex flex-col">
         <LanguageProvider>
           <ThemeModeProvider>
             <IThemeProvider>
               <IHeader />
+
               <FilteredDateProvider>
                 <main className="flex-1 overflow-y-auto -webkit-overflow-scrolling-touch mt-[100px] p-1 md:p-4 print:mt-0">
                   {children}
                 </main>
               </FilteredDateProvider>
+
               <GlobalToaster />
               <DialogContainer />
             </IThemeProvider>
